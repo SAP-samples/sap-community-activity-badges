e212bc77922e3abab76aa60b86495a8460ac57ed
Thomas Jung <thomas.jung@sap.com>
2024-02-04 02:44:03 -0500

    Profile Signature Enhancements - Scrolling, auto load profile, etc

--- srv/app/flp/profile/controller/App.controller.js
+++ srv/app/flp/profile/controller/App.controller.js
@@ -36,13 +36,73 @@ sap.ui.define([
                         oController.endBusy(oController)
                         let model = oController.getModel("profileModel")
                         let data = { scnId: model.getProperty("/scnId"), data: myJSON.data }
-                        if (data.data && data.data.user_badges) {
-                            for (let item of data.data.user_badges.items) {
-                                item.selected = ''
+
+                        data.signatureFull = data.data.signature
+                        const parser = new DOMParser()
+                        const htmlDoc = parser.parseFromString(data.signatureFull, 'text/html')
+                        const htmlElms = htmlDoc.getElementsByTagName('img')
+                        let src = ""
+                        for (const htmlElm of htmlElms) {
+                            src = htmlElm.getAttribute('src')
+                        }
+
+                        data.badge1 = ""
+                        data.badge1URL = ""
+                        data.badge2 = ""
+                        data.badge2URL = ""
+                        data.badge3 = ""
+                        data.badge3URL = ""
+                        data.badge4 = ""
+                        data.badge4URL = ""
+                        data.badge5 = ""
+                        data.badge5URL = ""
+                        if (src !== ""){
+                            const url = new URL(src)
+                            let pathname = url.pathname.split('/')
+                            if(pathname[3]){
+                                data.badge1 = pathname[3]
+                            }
+                            if(pathname[4]){
+                                data.badge2 = pathname[4]
+                            }
+                            if(pathname[5]){
+                                data.badge3 = pathname[5]
+                            }
+                            if(pathname[6]){
+                                data.badge4 = pathname[6]
+                            }
+                            if(pathname[7]){
+                                data.badge5 = pathname[7]
                             }
                         }
-                        for (let item of data.data.user_badges.items) {
-                            item.selected = ''
+                        if (data.data && data.data.user_badges) {
+                            for (let index = 0; index < data.data.user_badges.items.length; index++) {
+                                data.data.user_badges.items[index].selected = ''
+                                data.data.user_badges.items[index].earned_date =
+                                  new Date((typeof date === "string" ? new Date(data.data.user_badges.items[index].earned_date) : data.data.user_badges.items[index].earned_date) 
+                                )
+
+                                if(data.badge1 === data.data.user_badges.items[index].badge.id){
+                                    data.data.user_badges.items[index].selected = "true"
+                                    data.badge1URL = data.data.user_badges.items[index].badge.icon_url
+                                }
+                                if(data.badge2 === data.data.user_badges.items[index].badge.id){
+                                    data.data.user_badges.items[index].selected = "true"
+                                    data.badge2URL = data.data.user_badges.items[index].badge.icon_url
+                                }
+                                if(data.badge3 === data.data.user_badges.items[index].badge.id){
+                                    data.data.user_badges.items[index].selected = "true"
+                                    data.badge3URL = data.data.user_badges.items[index].badge.icon_url
+                                }
+                                if(data.badge4 === data.data.user_badges.items[index].badge.id){
+                                    data.data.user_badges.items[index].selected = "true"
+                                    data.badge4URL = data.data.user_badges.items[index].badge.icon_url
+                                }
+                                if(data.badge5 === data.data.user_badges.items[index].badge.id){
+                                    data.data.user_badges.items[index].selected = "true"
+                                    data.badge5URL = data.data.user_badges.items[index].badge.icon_url
+                                }
+                            }
                         }
                         model.setData(data)
                         oController.buildSignature()
@@ -54,18 +114,20 @@ sap.ui.define([
             },
 
             buildSignature: function () {
+
                 let model = this.getModel("profileModel")
                 let scnId = model.getProperty("/scnId")
                 let data = model.getData()
                 let signatureURL = `/showcaseBadgesGroups/${scnId}`
-                if (data.badge1) signatureURL += `/${data.badge1}` 
-                if (data.badge2) signatureURL += `/${data.badge2}` 
-                if (data.badge3) signatureURL += `/${data.badge3}` 
-                if (data.badge4) signatureURL += `/${data.badge4}` 
-                if (data.badge5) signatureURL += `/${data.badge5}`                 
+                if (data.badge1) signatureURL += `/${data.badge1}`
+                if (data.badge2) signatureURL += `/${data.badge2}`
+                if (data.badge3) signatureURL += `/${data.badge3}`
+                if (data.badge4) signatureURL += `/${data.badge4}`
+                if (data.badge5) signatureURL += `/${data.badge5}`
                 model.setProperty("/signatureURL", signatureURL)
+                model.setProperty("/signatureBigURL", signatureURL.replace("showcaseBadgesGroups", "showcaseBadges"))
                 let signatureFull = `<a href="${data.data.view_href}" target="_blank">` +
-                `<img src="https://devrel-tools-prod-scn-badges-srv.cfapps.eu10.hana.ondemand.com${signatureURL}" /></a>`
+                    `<img src="https://devrel-tools-prod-scn-badges-srv.cfapps.eu10.hana.ondemand.com${signatureURL}" /></a>`
                 model.setProperty("/signatureFull", signatureFull)
             },
 
